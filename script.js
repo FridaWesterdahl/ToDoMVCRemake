8977f8158f62bca38199aa697e4d7c5e0de1d4ed
Frida <fridaawesterdahl@gmail.com>
2022-02-25 14:40:47 +0100

    fixed clear all btn visibility

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let newNote;
 let itemsCount = 0;
 let notes = [];
+let checkboxCount = 0;
 
 
 document.addEventListener("submit", (event) => {
     event.preventDefault();
 
     itemsCount++;
-    console.log("item count:", itemsCount)
+    console.log("item count:", itemsCount);
+    itemsLeft();
+    clearAllBtn();
 
     notes.push(input.value);
     console.log("notes[]:", notes);
@@ -23,23 +26,41 @@ document.addEventListener("submit", (event) => {
     checkbox.onclick = () => {
         if (checkbox.checked) {
             itemsCount--;
+            checkboxCount++;
+            console.log("checkboxCount:", checkboxCount);
+            itemsLeft();
             note.style.color = "grey";
             note.style.textDecoration = "line-through";
             console.log("itemsCount when checked:", itemsCount);
-            note.id = "completed";
+            note.className = "completed";
+            clearAllBtn();
         }
         else {
             itemsCount++;
+            checkboxCount--;
+            console.log("checkboxCount:", checkboxCount);
+            itemsLeft();
             console.log("itemsCount when unchecked:", itemsCount);
             note.style.color = "black";
             note.style.textDecoration = "none";
+            clearAllBtn();
+        }
+    };
+
+    function clearAllBtn() {
+        const clearAll = document.querySelector(".clear-all");
+        if (checkboxCount < 1) {
+            clearAll.style.visibility = "hidden";     
+        }
+        else {
+            clearAll.style.visibility = "visible"; 
         }
     };
 
     let note = document.createElement("p");
     note.textContent = input.value;
     li.append(note);
-    note.id = "active";
+    note.className = "active";
 
     let removeBtn = document.createElement("button");
     removeBtn.className = "remove";
@@ -47,76 +68,44 @@ document.addEventListener("submit", (event) => {
     li.append(removeBtn);
     removeBtn.onclick = () => {
         li.remove();
-        itemsCount--;
-        console.log("itemsCount when removed:", itemsCount);
-    }  
-    
+
+        if (!checkbox.checked) {
+            itemsCount--;
+            itemsLeft();
+            console.log("itemsCount when removed:", itemsCount);
+        } 
+    };  
 
     document.querySelector("#input").value = "";
 });
 
+function itemsLeft() { 
+    if (itemsCount === 1) {
+        document.getElementById("p").innerHTML = itemsCount + " item left";
+    }
+    if (itemsCount > 1) {
+        document.getElementById("p").innerHTML = itemsCount + " items left";
+    }
+}
 
-function addNote() {
+    const allNotes = document.querySelector(".all-notes");
+    allNotes.onclick = () => {
+
+    };
+
+    const active = document.querySelector(".active");
+    active.onclick = () => {
+
+    };
+
+    const completed = document.querySelector(".completed");
+    completed.onclick = () => {
+
+    };
+
     
-}
 
-function activateFilter() {
-
-    let filter = document.createElement("div");
-    filter.className = "filter-notes";
-   
-    let items = document.createElement("p");
-    items.textContent = itemsCount + " items left";
-
-    let allBtn = document.createElement("button");
-    allBtn.textContent = "All";
-    allBtn.onclick = () => {
-        showAllNotes();
-    }
-
-    let activeBtn = document.createElement("button");
-    activeBtn.textContent = "Active";
-    activeBtn.onclick = () => {
-        showActive();
-    }
-
-    let completedBtn = document.createElement("button");
-    completedBtn.textContent = "Completed";
-    completedBtn.onclick = () => {
-        showCompleted();
-    }
-
-    let clearComBtn = document.createElement("button");
-    clearComBtn.className = "clearAll";
-    clearComBtn.textContent = "Clear completed";
-    clearComBtn.onclick = () => {
-        // li.remove();
-
-    }  
-
-    document.querySelector("#filter").append(filter);
-    filter.append(items);
-    filter.append(allBtn);
-    filter.append(activeBtn);
-    filter.append(completedBtn);
-    filter.append(clearComBtn);
-
-}
-
-function showAllNotes() {
-    // Show all notes in filter
-}
-
-function showActive() {
-    // Show active notes in filter
-}
-
-function showCompleted() {
-    // Show completed notes in filter
-}
-
-function markDone() {
-    console.log("markDone");
-
-
-}
+    const clearAll = document.querySelector(".clear-all");
+        clearAll.onclick = () => {
+        console.log("onclick clear :", checkboxCount)
+    };
