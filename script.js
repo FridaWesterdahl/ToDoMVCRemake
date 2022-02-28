4d723108becae7d4f0fa20a8489dda1891a76931
Frida <fridaawesterdahl@gmail.com>
2022-02-28 18:03:19 +0100

    update

--- script.js
+++ script.js
@@ -1,126 +1,229 @@
-let newNote;
-let itemsCount = 0;
-let notes = [];
-let checkboxCount = 0;
+let activeTasks = 0;
+let completedTasks = 0;
+let filterStatus = "all";
 
+start();
 
- function filters() {
-    let filter = document.querySelector("#filter");
-        if (itemsCount >= 1) {
-            filter.style.visibility = "visible";
-            console.log("filter itemCount:", itemsCount)
-        }
-        else {
-            filter.style.visibility = "hidden";
-        }
+function start() {
+    Input();
+    setupTemplate();
+    setupToggleAll();
+    setupClearCompleted();
+    setupShowAllButton();
+    setupShowActiveButton();
+    setupCompletedButton();
+}
 
- };
+function Input() {
+    let inputBar = document.querySelector(".input-bar");
+    let input = inputBar.querySelector("#input");
 
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    itemsCount++;
-    console.log("item count:", itemsCount);      
-    filters();
-    itemsLeft();
-    clearAllBtn();
-
-    notes.push(input.value);
-    console.log("notes[]:", notes);
-
-    let ul = document.querySelector(".todo-list");
-    let li = document.createElement("li");
-    ul.append(li);
-
-    let checkbox = document.createElement("INPUT");
-    checkbox.setAttribute("type", "checkbox");
-    li.append(checkbox);
-    console.log("input value:", input.value);
-    checkbox.onclick = () => {
-        if (checkbox.checked) {
-            itemsCount--;
-            checkboxCount++;
-            console.log("checkboxCount:", checkboxCount);
-            itemsLeft();
-            note.style.color = "grey";
-            note.style.textDecoration = "line-through";
-            console.log("itemsCount:", itemsCount);
-            note.className = "completed";
-            clearAllBtn();
-        }
-        else {
-            itemsCount++;
-            checkboxCount--;
-            console.log("checkboxCount:", checkboxCount);
-            itemsLeft();
-            console.log("itemsCount:", itemsCount);
-            note.style.color = "black";
-            note.style.textDecoration = "none";
-            clearAllBtn();
-        }
-        filters();
+    inputBar.onsubmit = event => {
+        console.log("input:", input.value);
+        event.preventDefault();
+        addNote(input.value);
+        updateButtons();
+        document.querySelector(".toggle-all").checked = false;
+        input.value = "";
     };
+}
 
-    let note = document.createElement("p");
-    note.textContent = input.value;
-    li.append(note);
-    note.className = "active";
+function addNote(text) {
+    let li = template.content.firstElementChild.cloneNode(true);  
+    let label = li.querySelector(".todo-text");
+    label.textContent = text;
 
-    let removeBtn = document.createElement("button");
-    removeBtn.className = "remove";
-    removeBtn.textContent = "❌";
-    li.append(removeBtn);
-    removeBtn.onclick = () => {
+    let doneButton = li.querySelector(".toggle");
+    let removeButton = li.querySelector(".remove-button");
+    removeButton.onclick = () => {
+        if (doneButton.checked) {
+            completedTasks--;
+        }
+        else {
+            activeTasks--;
+        }
         li.remove();
-        filters();
-        itemsLeft();
-        clearAllBtn();
-        if (!checkbox.checked) {
-            itemsCount--;
-            itemsLeft();
-            filters();
-            console.log("itemsCount:", itemsCount);
-        } 
-    };  
-
-    function clearAllBtn() {
-        const clearAll = document.querySelector(".clear-all");
-        if (checkboxCount < 1) {
-            clearAll.style.visibility = "hidden";     
-        }
-        else {
-            clearAll.style.visibility = "visible"; 
-        }
+        updateButtons();
     };
 
-    document.querySelector("#input").value = "";
-});
+    li.addEventListener("mouseenter", function (event) {
+        event.target.querySelector(".remove-button").classList.remove("hidden");
+    });
+    li.addEventListener("mouseleave", function (event) {
+        event.target.querySelector(".remove-button").classList.add("hidden");
+    });
 
-function itemsLeft() { 
-    if (itemsCount <= 1) {
-        document.getElementById("p").innerHTML = itemsCount + " item left";
+    doneButton.onclick = () => {
+        toggleDone(li, doneButton.checked);
+        updateButtons();
+    };
+
+    setItemViewStatus(li);
+    
+    let ul = document.querySelector("#todo-list");
+    ul.append(li);
+    activeTasks++;
+}
+
+function setupTemplate() {
+    template = document.querySelector("#todo-template");
+    template.remove();
+}
+
+function setupToggleAll() {
+    let checkBox = document.querySelector(".toggle-all");
+    checkBox.onclick = () => {
+        toggleAll(checkBox.checked);
+        updateButtons();
+    };
+}
+
+function setupShowAllButton() {
+    let button = document.querySelector("#all");
+    button.onclick = () => {
+        filterView("all");
+    };
+}
+
+function setupShowActiveButton() {
+    let button = document.querySelector("#active");
+    button.onclick = () => {
+        filterView("active");
+    };
+}
+
+function setupCompletedButton() {
+    let button = document.querySelector("#completed");
+    button.onclick = () => {
+        filterView("completed");
+    };
+}
+
+function setupClearCompleted() {
+    let button = document.querySelector("#clear-completed");
+    button.onclick = () => {
+        removeAllCompletedItems();
+        updateButtons();
+        document.querySelector(".toggle-all").checked = false;
+    };
+}
+
+function updateButtons() {
+    let todolist = document.querySelector("#todo-list");
+    let statusField = document.querySelector("#status-field");
+    let removeAllCompletedItemsButton = statusField.querySelector("#clear-completed");
+    let itemsLeft = statusField.querySelector("#active-tasks");
+
+    if (completedTasks > 0) {
+        removeAllCompletedItemsButton.classList.remove("hidden");
     }
-    if (itemsCount > 1) {
-        document.getElementById("p").innerHTML = itemsCount + " items left";
+    else {
+        removeAllCompletedItemsButton.classList.add("hidden");
+    }
+
+    if (completedTasks === 0 && activeTasks === 0) {
+        statusField.classList.add("hidden");
+        todolist.classList.add("hidden");
+    }
+    else {
+        statusField.classList.remove("hidden");
+        todolist.classList.remove("hidden");
+        itemsLeft.textContent = activeTasks;
     }
 }
 
-    const allNotes = document.querySelector(".all-notes");
-    allNotes.onclick = () => {
-        console.log("onclick all ;)")
-    };
+function filterView(newStatus) {
+    filterStatus = newStatus;
+    let liItems = document.querySelector("#todo-list").querySelectorAll("li");
+    for (const li of liItems) {
+        setItemViewStatus(li);
+    }
 
-    const active = document.querySelector(".active");
-    active.onclick = () => {
-        console.log("onclick active ;)") 
-    };
+    let allButton = document.querySelector("#all");
+    let activeButton = document.querySelector("#active");
+    let completedButton = document.querySelector("#completed");
 
-    const completed = document.querySelector(".completed");
-    completed.onclick = () => {
-        console.log("onclick completed ;)")
-    };
+    allButton.classList.remove("active");
+    activeButton.classList.remove("active");
+    completedButton.classList.remove("active");
 
-    const clearAll = document.querySelector(".clear-all");
-        clearAll.onclick = () => {
-        console.log("onclick clear ;)")
+    switch (newStatus) {
+        case "all":
+            allButton.classList.add("active");
+            break;
+        case "active":
+            activeButton.classList.add("active");
+            break;
+        case "completed":
+            completedButton.classList.add("active");
+            break;
+        default:
+            break;
     };
+}
+
+function setItemViewStatus(li) {
+    let hidden = "hidden";
+
+    switch (filterStatus) {
+        case "all":
+            li.classList.remove(hidden);
+            break;
+        case "active":
+            if (!li.classList.contains("completed")) {
+                li.classList.remove(hidden);
+            }
+            else {
+                li.classList.add(hidden);
+            }
+            break;
+        case "completed":
+            if (li.classList.contains("completed")) {
+                li.classList.remove(hidden);
+            }
+            else {
+                li.classList.add(hidden);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+function removeAllCompletedItems() {
+    let liItems = document.querySelector("#todo-list").querySelectorAll("li");
+    for (const li of liItems) {
+        let checkBox = li.querySelector(".toggle");
+        if (checkBox.checked) {
+            completedTasks--;
+            li.remove();
+        }
+    }
+}
+
+function toggleAll(isChecked) {
+    let liItems = document.querySelector("#todo-list").querySelectorAll("li");
+
+    for (const li of liItems) {
+        let toggle = li.querySelector(".toggle");
+        if (toggle.checked != isChecked) {
+            toggle.checked = isChecked;
+            toggleDone(li, isChecked);
+        }
+    }
+}
+
+function toggleDone(li, isChecked) {
+    if (isChecked) {
+        li.classList.add("completed");
+        completedTasks++;
+        activeTasks--;
+    }
+    else if (!isChecked) {
+        li.classList.remove("completed");
+        completedTasks--;
+        activeTasks++;
+    }
+    setItemViewStatus(li);
+}
+
